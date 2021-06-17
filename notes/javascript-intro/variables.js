430d885b122608a9fbe4e9122bd466d5edc91ef8
Ben <brbryant2639@gmail.com>
2021-06-17 10:42:49 -0500

    Variable/value notes updated

---
+++ notes/javascript-intro/variables.js
@@ -0,0 +1,54 @@
+// var, function scoped
+
+var age = 30;
+
+// let, block scoped
+
+let cartons = 5;
+
+// Re-assign variable values
+
+cartons = 4;
+
+// const, block scoped
+
+const doesNotChange = true;
+
+// Declaring Variables
+
+var age;
+
+// Assigning values to variables
+
+var age = 30;
+
+// Re-declare a variable
+
+// can re-declare a variable using var
+var age = "age";
+
+// cannot re-declare using let or const
+const age = 45;
+
+// Camel Case for names with more than one word
+let thisIsAnExampleOfCamelCase = true;
+
+// Value Types
+
+// Number
+let numOfBottles = 7;
+
+// String
+const firstName = "Ben";
+
+// Boolean
+let isOnline = false;
+
+// Undefined
+let message; // undefined
+
+// Null
+let middleInitial = null;
+
+// BigInt
+let largeNumber = 12345678901234567890n;
