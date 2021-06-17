fa0c1500f8f06387fcc554f6c9897d2983636ab1
Ben <brbryant2639@gmail.com>
2021-06-17 14:11:06 -0500

    Added equalities

--- notes/javascript-intro/operators.js
+++ notes/javascript-intro/operators.js
@@ -1,39 +1,51 @@
 // Arithmetic Operators
 
 // Addition
-1 + 1
+1 + 1;
 
 // Subtraction
-1 - 1
+1 - 1;
 
 // Multiplication
-1 * 2
+1 * 2;
 
 // Division
-2 / 2
+2 / 2;
 
 // Modulus (Division Remainder)
-2 % 2 // 0
+2 % 2; // 0
 
 // Assigment Operators
-let x = 3
+let x = 3;
 
 // Comparison Operators
 
 // == 'Loose' equality
-1 == 1
+1 == 1;
 
 // === 'Strict' equality
-1 === 1
+1 === 1;
+
+// != 'Loose' inequality
+2 != '2' // false
+
+// !== 'Strict' inequality
+2 !== 2 // false
 
 // > Greater than
-2 > 1
+2 > 1;
 
 // < Lesser than
-2 < 1
+2 < 1;
 
 // >= Greater than or equal to
-2 >= 1
+2 >= 1;
 
 // Lesser than or equal to
-2 <= 1
+2 <= 1;
+
+let age = 30;
+let ageAsString = "30";
+
+console.log(age == ageAsString); // true
+console.log(age === ageAsString); // false
