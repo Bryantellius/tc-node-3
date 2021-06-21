aae373bf80980065af1e1b5535c0a811dd4f97b3
Ben <brbryant2639@gmail.com>
2021-06-21 15:03:10 -0500

    Added function walkthrough

--- notes/javascript-intro/intro.js
+++ notes/javascript-intro/intro.js
@@ -1,29 +1,15 @@
-// Ignore Even
-// Count from 100 to 1
-// Print even numbers
+// Assign function to a variable
+let isEven = function (number) {
+  return number % 2 == 0;
+};
 
-for (let i = 100; i > 1; i -= 2) {
-  console.log(i);
+let answer = isEven(25); // false
+
+// Declare a function
+function printThreeNames(name, name2, name3 = "Unknown") {
+  console.log(name, name2, name3);
 }
 
-// FIZZ BUZZ
-// Count from 1 to 100
-// Print FIZZ if number is divisible by 3
-// Print BUZZ if number is divisible by 5
-// Print FIZZBUZZ if number is divisible by 3 and 5
+let answer = isEven(18577295783084); // true
 
-for (let i = 1; i <= 100; i++) {
-  // iterate from 1 to 100
-  let output = "";
-
-  // Use mod (division remainder) to find if any number is divisible (evenly) by another number
-  if (i % 3 == 0) {
-    output += "FIZZ";
-  }
-
-  if (i % 5 == 0) {
-    output += "BUZZ";
-  }
-
-  console.log(`${i} ${output}`);
-}
+let result = printThreeNames("Ben", "Seth", "Cruz"); // undefined
