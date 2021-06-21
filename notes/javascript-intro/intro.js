797ed21ca0ff550aeb3723d961854f793a630392
Ben <brbryant2639@gmail.com>
2021-06-21 10:03:43 -0500

    Loop review

--- notes/javascript-intro/intro.js
+++ notes/javascript-intro/intro.js
@@ -1,34 +1,10 @@
-// This is a comment
-// Comments start with slashes and are ignored by the computer
+// Loops
 
-// Let's declare a variable (something that stores a value)
-var variableName = "value";
-let variablename = 1;
-const variable_name = true;
+let continueLoopWhileAbove0 = 10;
 
-// Here is syntax for control flow (if/else)
+// While
 
-if (variable_name) {
-  // do something
-  console.log("variable_name's value evaluated to true");
-} else {
-  // do something else
-  console.log("variable_name's value evaluated to false");
-}
-
-// Here is syntax for a loop (something that runs over and over again until it should stop
-
-// while loop
-while (variable_name) {
-  // do something repeatedly
-}
-
-// do while loop
-do {
-  // do something repeatedly
-} while (variable_name);
-
-// for loop
-for (let index = 0; index < 10; index++) {
-  // do something repeatedly
+while (10 == true) {
+  console.log(continueLoopWhileAbove0);
+  continueLoopWhileAbove0--;
 }
