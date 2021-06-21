b5fd3f5e97690df6f74ac3b8ef0c77d0b7e72cfa
Ben <brbryant2639@gmail.com>
2021-06-21 10:17:25 -0500

    Added for loop review

--- notes/javascript-intro/intro.js
+++ notes/javascript-intro/intro.js
@@ -3,8 +3,27 @@
 let continueLoopWhileAbove0 = 10;
 
 // While
+// Will loop 0 or more times
 
-while (10 == true) {
+while (continueLoopWhileAbove0) {
   console.log(continueLoopWhileAbove0);
   continueLoopWhileAbove0--;
+}
+
+// Do/While
+// Will loop 1 or more times
+
+do {
+  console.log(continueLoopWhileAbove0);
+  continueLoopWhileAbove0--;
+} while (continueLoopWhileAbove0);
+
+// For
+
+for (
+  let continueLoopWhileAbove0 = 10;
+  continueLoopWhileAbove0;
+  continueLoopWhileAbove0--
+) {
+  console.log(continueLoopWhileAbove0);
 }
