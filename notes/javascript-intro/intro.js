b19b5b68d9f3a5bea19ce09dda6f813c9d517b04
Ben <brbryant2639@gmail.com>
2021-06-21 13:39:03 -0500

    Added FIZZBUZZ

--- notes/javascript-intro/intro.js
+++ notes/javascript-intro/intro.js
@@ -1,29 +1,29 @@
-// Loops
+// Ignore Even
+// Count from 100 to 1
+// Print even numbers
 
-let continueLoopWhileAbove0 = 10;
-
-// While
-// Will loop 0 or more times
-
-while (continueLoopWhileAbove0) {
-  console.log(continueLoopWhileAbove0);
-  continueLoopWhileAbove0--;
+for (let i = 100; i > 1; i -= 2) {
+  console.log(i);
 }
 
-// Do/While
-// Will loop 1 or more times
+// FIZZ BUZZ
+// Count from 1 to 100
+// Print FIZZ if number is divisible by 3
+// Print BUZZ if number is divisible by 5
+// Print FIZZBUZZ if number is divisible by 3 and 5
 
-do {
-  console.log(continueLoopWhileAbove0);
-  continueLoopWhileAbove0--;
-} while (continueLoopWhileAbove0);
+for (let i = 1; i <= 100; i++) {
+  // iterate from 1 to 100
+  let output = "";
 
-// For
+  // Use mod (division remainder) to find if any number is divisible (evenly) by another number
+  if (i % 3 == 0) {
+    output += "FIZZ";
+  }
 
-for (
-  let continueLoopWhileAbove0 = 10;
-  continueLoopWhileAbove0;
-  continueLoopWhileAbove0--
-) {
-  console.log(continueLoopWhileAbove0);
+  if (i % 5 == 0) {
+    output += "BUZZ";
+  }
+
+  console.log(`${i} ${output}`);
 }
