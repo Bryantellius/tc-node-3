9bc4890bb222ffc956d76a3d2de86e36fa680e0a
Ben <brbryant2639@gmail.com>
2021-06-25 08:14:19 -0500

    Removed export modules for classes

--- notes/oop/more.js
+++ notes/oop/more.js
@@ -1,5 +1,59 @@
-const CookBook = require("./classes/Cookbook");
-const Recipe = require("./classes/Recipe");
+class CookBook {
+  constructor() {
+    this.recipes = [];
+  }
+
+  addRecipe(recipe) {
+    this.recipes.push(recipe);
+  }
+
+  read(recipe) {
+    if (recipe) {
+      let found;
+      this.recipes.forEach(function (value) {
+        if (value.name == recipe) {
+          found = value;
+        }
+      });
+      if (found) {
+        found.displayIngredients();
+        found.displayDirections();
+      } else {
+        console.log("Could not find the recipe you were looking for :(");
+      }
+    } else {
+      this.recipes.forEach(function (recipe, idx) {
+        console.log(`${idx + 1} | ${recipe.name}`);
+      });
+    }
+  }
+}
+
+class Recipe {
+  constructor(name, category, ingredients, duration, directions, servings) {
+    this.name = name;
+    this.category = category;
+    this.ingredients = ingredients;
+    this.duration = duration;
+    this.directions = directions;
+    this.servings = servings;
+    this.rating = null;
+  }
+
+  rate(liked, total) {
+    this.rating = `${liked}/${total}`;
+  }
+
+  displayIngredients() {
+    this.ingredients.forEach(function (value) {
+      console.log(`${value.measurement} of ${value.name}`);
+    });
+  }
+
+  displayDirections() {
+    console.log(this.directions);
+  }
+}
 
 let cookbook = new CookBook();
 
