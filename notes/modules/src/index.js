21476e19fbc1099e158ef6576e66c13081a1e077
Ben <brbryant2639@gmail.com>
2021-07-01 14:54:44 -0500

    Added to walkthrough

--- notes/modules/src/index.js
+++ notes/modules/src/index.js
@@ -1,9 +1,20 @@
 import dayjs from "dayjs";
+import { DOMCreate, DOMStyle } from "./dom.utils";
+import math from "./math.utils";
+
+let subtotal = 19.99;
+let tax = 0.15;
 
 let today = dayjs();
 
-let h1 = document.createElement("h1");
+let h1 = DOMCreate("h1", "container", today.format("MM-DD-YYYY"));
+let p = DOMCreate(
+  "p",
+  "container",
+  `Subtotal: ${subtotal} with tax: ${tax * 100}%`
+);
+let total = DOMCreate("p", "container", `Total: $${math.salesTax(subtotal, tax)}`);
 
-h1.textContent = today.format("MM-DD-YYYY");
-
-document.body.appendChild(h1);
+DOMStyle(h1, "font-size: 100px");
+DOMStyle(p, "text-decoration: underline");
+DOMStyle(total, "color: green");
