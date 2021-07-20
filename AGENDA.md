77ec38627f9b0046848d65a44086da1acd4743ed
Ben <brbryant2639@gmail.com>
2021-07-20 10:15:04 -0500

    Added node intro md to edit

--- AGENDA.md
+++ AGENDA.md
@@ -1,32 +1,8 @@
-# July 16, 2021
+# July 20, 2021
 
 ## To Do
 
-1. JS Interview Questions
-2. SQL Intro Exercise
-3. SQL Joins Lecture
-4. SQL Joins Exercise
-
-## JS Interview Questions
-
-1. What are the different data types present in javascript?
-2. What is the difference between “==” and “===” operators.
-3. Explain Implicit Type Coercion in javascript.
-4. Is javascript a statically typed or a dynamically typed language?
-5. Explain how to declare a variable and assign it a value.
-6. Explain Hoisting in javascript.
-7. What is the DOM?
-8. What are object prototypes?
-9. What is the use of a constructor function in javascript?
-10. Explain “this” keyword.
-11. What is NaN property in JavaScript?
-12. Explain passed by value and passed by reference.
-13. Explain Higher Order Functions in javascript.
-14. What are callbacks?
-15. What is recursion in a programming language?
-16. Explain Closures in JavaScript.
-17. Explain Scope and Scope Chain in javascript.
-18. Explain call(), apply() and, bind() methods.
-19. What is Currying in javascript?
-20. What is an Immediately Invoked Function in javascript?
-21. What is memoization?
+1. Binary Master Class
+2. JS Coding Problems
+3. Node Intro Lecture
+4. Node Intro Exercise
--- interview-answers.js
+++ interview-answers.js
@@ -1,13 +1,13 @@
 //What are the different data types present in javascript?
 
 // Primitive Data Types
-'string'
-true
-12
-1n
-;('symbol')
-undefined
-null
+"string";
+true;
+12;
+1n;
+("symbol");
+undefined;
+null;
 
 // Non-primitive (structured) Data Type
 {
@@ -15,34 +15,34 @@ null
 function something() {}
 
 //What is the difference between “==” and “===” operators.
-'1' == 1 // true
-'1' === 1 // false
+"1" == 1; // true
+"1" === 1; // false
 
 //Explain Implicit Type Coercion in javascript.
-'1' == 1
-1 == 1
-'1' == '1'
+"1" == 1;
+1 == 1;
+"1" == "1";
 
 //Is javascript a statically typed or a dynamically typed language?
-let x = 1 // number type
-x = '1' // string type
+let x = 1; // number type
+x = "1"; // string type
 
 //Explain how to declare a variable and assign it a value.
-let varName = 1 // most common
-var _varname = 1
-const $100 = 1
+let varName = 1; // most common
+var _varname = 1;
+const $100 = 1;
 
 //What are the naming conventions for variable names?
 
 //Explain Hoisting in javascript.
 
 //What is the DOM?
-let dom = 'Document Object Model'
+let dom = "Document Object Model";
 
 // <h1 class="title pageHeading" id="titleHeading">Heading</h1>
 let h1 = {
-  textContent: 'Heading',
-}
+  textContent: "Heading",
+};
 
 //What are object prototypes?
 
@@ -50,16 +50,16 @@ let h1 = {
 // Allows inheritence (properties and methods) from prototypes to instances
 
 function talk(message) {
-  console.log(this.name + ' ' + message)
+  console.log(this.name + " " + message);
 }
 
 function Person(name) {
-  this.name = name
-  this.speak = talk
-  console.log(this)
+  this.name = name;
+  this.speak = talk;
+  console.log(this);
 }
 
-new Person('Ben').speak('says hello.')
+new Person("Ben").speak("says hello.");
 
 //What is the use of a constructor function in javascript?
 
@@ -67,8 +67,8 @@ new Person('Ben').speak('says hello.')
 
 class Student {
   constructor(name) {
-    this.name = name
-    console.log(this)
+    this.name = name;
+    console.log(this);
   }
 }
 
@@ -87,25 +87,23 @@ class Student {
 
 //What are callbacks?
 
-let arr = [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5];
 
 function map(callback) {
-  let copy = []
+  let copy = [];
   for (let index = 0; index < arr.length; index++) {
-    copy.push(callback(arr[index], index, arr))
+    copy.push(callback(arr[index], index, arr));
   }
-  return copy
+  return copy;
 }
 
 function callback(val, idx, arr) {
-  return val ** idx
+  return val ** idx;
 }
 
-console.log(map(callback))
+console.log(map(callback));
 
-console.log(arr.map(callback))
-
-
+console.log(arr.map(callback));
 
 let btn = document.querySelector("#btn");
 let feedback = document.querySelector("#time");
@@ -115,16 +113,16 @@ let count = 0;
 let timer = setInterval(() => {
   if (time == 0) {
     clearInterval(timer);
-    feedback.textContent = "Wow, you clicked the button " + count + " times!"
+    feedback.textContent = "Wow, you clicked the button " + count + " times!";
     btn.disabled = true;
     return;
   }
-  feedback.textContent = --time
+  feedback.textContent = --time;
 }, 100);
 
 btn.addEventListener("click", () => {
   btn.textContent = "Clicked " + ++count;
-})
+});
 
 // Function value that you pass to a HOF, that is called after some action finishes
 
